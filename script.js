a99d79e1f92b304696e4adfcb41867874a30ab0e
damianrobert <damianrobert666@yahoo.com>
2022-09-23 15:57:32 +0300

    updated all 3 files

---
+++ script.js
@@ -0,0 +1,99 @@
+const calculator = document.querySelector('.calculator')
+let screenLast = document.querySelector('.screenLast')
+let screenCurrent = document.querySelector('.screenCurrent')
+const buttons = document.querySelector('.allButtons')
+const button = buttons.querySelectorAll('BUTTON')
+const operators = document.querySelectorAll('.operators')
+
+let firstOperand
+let secondOperand
+let selectedOperator
+let result
+let key
+let keyValue
+
+function calculate(first, operator, second){
+    if(operator === '+'){
+       first + second
+    } else  if(operator === '-'){
+        first - second
+    } else  if(operator === '÷'){
+        first / second
+    } else  if(operator === 'x'){
+        first * second
+    } else  if(operator === '%'){
+        first % second
+    }
+}
+
+
+button.forEach(bttn =>{
+    bttn.addEventListener('click', (e) => {
+         key = e.target
+         keyValue = key.innerText
+        if(key.className === 'digits'){
+            screenCurrent.innerText = screenCurrent.innerText + keyValue
+
+        } 
+        
+        if(key.className === 'operators'){
+            firstOperand = screenCurrent.innerText
+            screenLast.innerText = screenCurrent.innerText + keyValue 
+            selectedOperator = keyValue
+            console.log(selectedOperator)
+            screenCurrent.innerText = ''
+        }
+         
+        if(screenCurrent.innerText.includes('.')){
+            return
+        } else if(key.className === 'decimal'){
+            screenCurrent.innerText = screenCurrent.innerText + keyValue
+        }
+        
+    })
+})
+
+
+button.forEach(bttn => {
+    bttn.addEventListener('click', () =>{
+        if(key.className === 'equals'){
+            secondOperand = screenCurrent.innerText
+            firstOperand = parseInt(firstOperand)
+            secondOperand = parseInt(secondOperand)
+            if(selectedOperator === '+'){
+                result = firstOperand + secondOperand
+                screenLast.innerText = result
+                screenCurrent.innerText = ''
+            } else if(selectedOperator === '-'){
+                result = firstOperand - secondOperand
+                screenLast.innerText = result
+                screenCurrent.innerText = ''
+            } else if(selectedOperator === 'x'){
+                result = firstOperand * secondOperand
+                screenLast.innerText = result
+                screenCurrent.innerText = ''
+            } else if(selectedOperator === '÷'){
+                result = firstOperand / secondOperand
+                screenLast.innerText = result
+                screenCurrent.innerText = ''
+            } else if(selectedOperator === '%'){
+                result = firstOperand % secondOperand
+                screenLast.innerText = result
+                screenCurrent.innerText = ''
+            }
+        }
+    })
+})
+
+
+/*if(selectedOperator === '+'){
+                screenLast.innerText = parseInt(firstOperand) + parseInt(secondOperand)
+            } else  if(selectedOperator === '-'){
+                screenLast.innerText = parseInt(firstOperand) - parseInt(secondOperand)
+            } else  if(selectedOperator === '÷'){
+                screenLast.innerText = parseInt(firstOperand) / parseInt(secondOperand)
+            } else  if(selectedOperator === 'x'){
+                screenLast.innerText = parseInt(firstOperand) * parseInt(secondOperand)
+            } else  if(selectedOperator === '%'){
+                screenLast.innerText = parseInt(firstOperand) % parseInt(secondOperand)
+            }*/
